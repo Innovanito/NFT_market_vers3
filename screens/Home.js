5ec6381f231ff7ef7584771a1f2d7befe7880b51
김하경 <khg05311@naver.com>
2022-04-30 11:06:07 +0900

    NFT data on the screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,24 @@
-import { Text} from 'react-native'
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
