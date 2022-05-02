3688317d97bfaecfdc3eda30faf437d1baee6acc
김하경 <khg05311@naver.com>
2022-05-02 19:14:51 +0900

    Details screen under processing1

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const App = () => {
         initialRouteName="Home"
       >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details}/>
+        <Stack.Screen name="Details" component={Details}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
