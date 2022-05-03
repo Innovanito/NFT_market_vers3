ea4ad8b945e256eb8bbda4deb3d206c4391c54d6
김하경 <khg05311@naver.com>
2022-05-03 12:04:43 +0900

    NFTDetails done

--- screens/Details.js
+++ screens/Details.js
@@ -10,6 +10,21 @@ const DetailsHeader = ({data, navigation}) => (
   <View style={{ width: '100%', height:373}}>
     <Image
       source={data.image}
+      resizeMode="cover"
+      style={{ width: '100%', height: '100%'}}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+    <CircleButton
+      imgUrl={assets.heart}
+      handlePress={() => navigation.goBack()}
+      right={15}
+      top={StatusBar.currentHeight + 10}
     />
   </View>
 )
@@ -22,7 +37,7 @@ const Details = ({route, navigation}) => {
       <FocusedStatusBar
         barStyle="dark-content"
         backgroundColor='transparent'
-        translucen={true}
+        translucent={true}
       />
       <View style={{
         width: '100%',
@@ -45,6 +60,19 @@ const Details = ({route, navigation}) => {
         ListHeaderComponent={() => (
           <React.Fragment>
             <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ padding: SIZES.font}}>
+              <DetailsDesc data={data} />
+              {data.bids.length > 0 && (
+                <Text style={{
+                  fontSize: SIZES.font,
+                  fontFamily: FONTS.semiBold,
+                  color: COLORS.primary
+                }}>
+                Current Bids
+                </Text>
+              )}
+            </View>
           </React.Fragment>
         )}
       />
